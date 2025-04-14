68975b2d119769eec0eb30a5f8560df448eb2082
petraalpigiano <petraalpigiano@gmail.com>
2025-04-14 15:39:31 +0200

    add array

---
+++ Snack 1.js
@@ -0,0 +1,22 @@
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore.
+
+const bike = [
+  {
+    name: bici1,
+    weight: 300,
+  },
+  {
+    name: bici2,
+    weight: 274,
+  },
+  {
+    name: bici3,
+    weight: 140,
+  },
+  {
+    name: bici4,
+    weight: 463,
+  },
+];
